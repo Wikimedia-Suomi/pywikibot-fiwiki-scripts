14cf7f83d5ce240cd08175dbd8f37048a233454f
Ilkka Prusi <ilkka.prusi@gmail.com>
2023-03-28 03:27:23 +0300

    more generic, less duplication, better accuracy

--- scripts/auktorit.py
+++ scripts/auktorit.py
@@ -29,49 +29,33 @@ def addnewline(oldtext):
         return oldtext.replace("iitteet}}", "iitteet}}\n")
 
 # ei tyhjää riviä viitemallineen ja tynkämallineen välissä? -> lisätään
-def nonewline(oldtext):
-    index = oldtext.find("{{tynkä")
-    if (index < 0):
+def nonewlinetemplate(oldtext,template):
+    # find template..
+    indextemp = oldtext.find(template)
+    if (indextemp < 0):
         return oldtext
-    strlen = len("iitteet}}\n")
-    sub = oldtext[index:-strlen]
-    if (sub == "iitteet}}\n"):
-        return oldtext.replace("iitteet}}\n{{tynkä", "iitteet}}\n\n{{tynkä")
-    return oldtext
 
-# sama kuin yllä paitsi iso T..
-def nonewlineT(oldtext):
-    index = oldtext.find("{{Tynkä")
-    if (index < 0):
-        return oldtext
-    strlen = len("iitteet}}\n")
-    sub = oldtext[index:-strlen]
-    if (sub == "iitteet}}\n"):
-        return oldtext.replace("iitteet}}\n{{Tynkä", "iitteet}}\n\n{{Tynkä")
-    return oldtext
+    # find where reference-template is and which type it is
+    reftext = "iitteet}}"
+    indexref = oldtext.find(reftext)
+    if (indexref < 0):
+        reftext = "iitteet|sarakkeet}}"
+        indexref = oldtext.find(reftext)
+        if (indexref < 0):
+            #print("did not find referencetemplate")
+            return oldtext
 
-# ei tyhjää riviä viitemallineen ja tynkämallineen välissä? -> lisätään
-def nonewlinecol(oldtext):
-    index = oldtext.find("{{tynkä")
-    if (index < 0):
-        return oldtext
-    strlen = len("iitteet|sarakkeet}}\n")
-    sub = oldtext[index:-strlen]
-    if (sub == "iitteet|sarakkeet}}\n"):
-        return oldtext.replace("iitteet|sarakkeet}}\n{{tynkä", "iitteet|sarakkeet}}\n\n{{tynkä")
+    # check if there is only one linechange separating
+    singleline = reftext + "\n" + template;
+    strlen = len(singleline)
+    sub = oldtext[indexref:indexref+strlen]
+    if (sub == singleline):
+        # output start + newline + rest (can't modify otherwise)
+        #print("adding linechange")
+        return oldtext[:indextemp] + "\n" + oldtext[indextemp:]
+        
+    #print("no changes, oldtext")
     return oldtext
-
-# sama kuin yllä paitsi iso T..
-def nonewlinecolT(oldtext):
-    index = oldtext.find("{{Tynkä")
-    if (index < 0):
-        return oldtext
-    strlen = len("iitteet|sarakkeet}}\n")
-    sub = oldtext[index:-strlen]
-    if (sub == "iitteet|sarakkeet}}\n"):
-        return oldtext.replace("iitteet|sarakkeet}}\n{{Tynkä", "iitteet|sarakkeet}}\n\n{{Tynkä")
-    return oldtext
-
 
 # ei tynkämallinetta tai muuta? -> etsitään luokka ja lisätään sitä ennen
 def insertaboveclass(oldtext):
@@ -90,9 +74,11 @@ site = pywikibot.Site("fi", "wikipedia")
 site.login()
 
 # haku auktoriteettitunnisteiden luettelossa olevilla
-url = "https://petscan.wmflabs.org/?psid=24596389"
+
+# scopus url = "https://petscan.wmflabs.org/?psid=24596657"
+url = "https://petscan.wmflabs.org/?psid=24602545"
 url += "&format=json"
-url += "&output_limit=10"
+url += "&output_limit=5"
 response = urlopen(url)
 data_json = json.loads(response.read())
 
@@ -100,6 +86,8 @@ for row in data_json['*'][0]['a']['*']:
     page=pywikibot.Page(site, row['title'])
     oldtext=page.text
     
+    print(" ======== ")
+    print("Editing [ " + row['title'] + " ]")
     if (oldtext.find("{{Auktoriteettitunnisteet") > 0 or oldtext.find("{{auktoriteettitunnisteet") > 0):
         print("Skipping " + row['title'] + " - auktoriteetit already added.")
         continue
@@ -108,32 +96,32 @@ for row in data_json['*'][0]['a']['*']:
     #pywikibot.showDiff(oldtext, temptext,2)
 
     temptext = addnewline(oldtext)
-    pywikibot.showDiff(oldtext, temptext,2)
-
-    #temptext = nonewline(temptext)
-    #pywikibot.showDiff(oldtext, temptext,2)
-    #temptext = nonewlineT(temptext)
-    #pywikibot.showDiff(oldtext, temptext,2)
-    #temptext = nonewlinecol(temptext)
-    #pywikibot.showDiff(oldtext, temptext,2)
-    #temptext = nonewlinecolT(temptext)
-    #pywikibot.showDiff(oldtext, temptext,2)
+    
+    #if oldtext != temptext:
+    #    pywikibot.showDiff(oldtext, temptext,2)
     
 # onko käännösmallinetta tai tynkämallinetta? jos ei kumpaakaan, onko aakkostusmallinetta?
 # jos ei ole sitäkään etsi luokka ja lisää sen ylle
     if (temptext.find("{{Käännös") > 0):
+        temptext = nonewlinetemplate(temptext,"{{Käännös")
         temptext = insertabovetemplate(temptext,"{{Käännös")
     elif (temptext.find("{{käännös") > 0):
+        temptext = nonewlinetemplate(temptext,"{{käännös")
         temptext = insertabovetemplate(temptext,"{{käännös")
     elif (temptext.find("{{Tynkä") > 0):
+        temptext = nonewlinetemplate(temptext,"{{Tynkä")
         temptext = insertabovetemplate(temptext,"{{Tynkä")
     elif (temptext.find("{{tynkä") > 0):
+        temptext = nonewlinetemplate(temptext,"{{tynkä")
         temptext = insertabovetemplate(temptext,"{{tynkä")
     elif (temptext.find("{{OLETUSAAKKOSTUS") > 0):
+        temptext = nonewlinetemplate(temptext,"{{OLETUSAAKKOSTUS")
         temptext = insertabovetemplate(temptext,"{{OLETUSAAKKOSTUS")
     elif (temptext.find("{{AAKKOSTUS") > 0):
+        temptext = nonewlinetemplate(temptext,"{{AAKKOSTUS")
         temptext = insertabovetemplate(temptext,"{{AAKKOSTUS")
     elif (temptext.find("{{DEFAULTSORT") > 0):
+        temptext = nonewlinetemplate(temptext,"{{DEFAULTSORT")
         temptext = insertabovetemplate(temptext,"{{DEFAULTSORT")
     else:
         temptext = insertaboveclass(temptext)
@@ -146,6 +134,10 @@ for row in data_json['*'][0]['a']['*']:
     pywikibot.showDiff(oldtext, temptext,2)
     summary='Lisätään auktoriteettitunnisteet -malline'
     pywikibot.info('Edit summary: {}'.format(summary))
+
+    if site.userinfo['messages']:
+        print("Warning: Talk page messages. Exiting.")
+        exit()
 
     question='Do you want to accept these changes?'
     choice = pywikibot.input_choice(
